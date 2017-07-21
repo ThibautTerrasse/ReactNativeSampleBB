5d3f496f044cc3773c96569ec2dc7655fde1f2a0
Thibaut Terrasse <thibautterrasse@hotmail.fr>
2017-07-21 12:43:41 +0200

    ajout d'un bouton d'alerte

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,6 +4,7 @@ import {
   Button,
   StyleSheet,
   View,
+  Alert,
 } from 'react-native';
 
 import Info from '../components/Info';
@@ -70,6 +71,18 @@ export default class HomeScreen extends Component {
             onPress={this.navigateToState}
             title="State"
           />
+        </View>
+        <View style={styles.margin}>
+          <TouchableHighlight
+            onPress={() => Alert.alert(
+              'Alert Title',
+              'Thibaut Terrasse',
+            )}
+          >
+            <View>
+              <Text>Alert with one button</Text>
+            </View>
+          </TouchableHighlight>
         </View>
       </ScrollView>
     );
